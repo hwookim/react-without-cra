b1a036908c21618afbf31072318bd93d9f75568b
hwookim <gusdn9754@gmail.com>
2021-08-17 13:01:24 +0900

    refactor: 테스트 폴더 분리

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   testMatch: [
     "<rootDir>/**/*.test.(js|jsx|ts|tsx)",
-    "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
+    "<rootDir>/test/**/*.test.(js|jsx|ts|tsx)",
   ],
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "./App";
+import App from "../src/App";
 import { render } from "@testing-library/react";
 
 describe("App", () => {
